6e1c91139a827c80d7371e1ff83d1ca0e1d4956d
deepanshu <deepanshukamlesh@gamil.com>
2024-08-18 15:24:52 +0530

    prisma setup done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
   const app = express();
@@ -16,11 +17,42 @@ async function init() {
             hello: String
             say (name: String): String 
         }
+        type Mutation{
+            createUser(firstName: String!,lastName: String!,email: String!,password: String!) : Boolean
+        }
     `,
     resolvers: {
       Query: {
         hello: () => "Happy Raksha Bandhan",
-        say: (_ , {name}:{name:String}) => `Hey ${name} , Jai Shree Ram`
+        say: (_, { name }: { name: String }) => `Hey ${name} , Jai Shree Ram`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              firstName,
+              lastName,
+              email,
+              password,
+              salt: "rainbow_salt",
+            },
+          });
+
+          return true;
+        },
       },
     },
   });
